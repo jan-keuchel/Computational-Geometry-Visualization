035fdb2c418edd50f9e851d681253c4ba978b386
Jan Keuchel <jan@keuchel.de>
2025-09-30 13:39:04 +0200

    Add direction to edge.

--- edge.py
+++ edge.py
@@ -6,12 +6,13 @@ import pygame
 class Edge:
     _next_id = 0
 
-    def __init__(self, a:Node, b:Node) -> None:
+    def __init__(self, a:Node, b:Node, is_directed=False) -> None:
         self.id = Edge._next_id
         Edge._next_id += 1
 
         self.a  = a
         self.b  = b
+        self.is_directed = is_directed
 
     def draw(self, screen) -> None:
         pygame.draw.aaline(screen, 
