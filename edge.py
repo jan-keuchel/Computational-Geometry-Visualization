4d71afb7faa07231d14ef64cec789a071b07e027
Jan Keuchel <jan@keuchel.de>
2025-10-01 13:18:08 +0200

    add weight.

--- edge.py
+++ edge.py
@@ -1,12 +1,18 @@
 # pyright: reportMissingImports=false
 import constants
+import math_helper
 from node import Node
 import pygame
 
 class Edge:
-    _next_id = 0
+    _next_id: int = 0
 
-    def __init__(self, a:Node, b:Node, is_directed=False) -> None:
+    def __init__(self, 
+                 a:Node,
+                 b:Node,
+                 weight=None,
+                 is_directed=False) -> None:
+
         self.id = Edge._next_id
         Edge._next_id += 1
 
@@ -14,10 +20,16 @@ class Edge:
         self.b  = b
         self.is_directed = is_directed
 
+        if weight == None:
+            self.weight = math_helper.distance(a.p, b.p)
+        else:
+            self.weight = weight
+
+
     def draw(self, screen) -> None:
         pygame.draw.aaline(screen, 
                            constants.FOREGROUND,
-                           (self.a.x, self.a.y), 
-                           (self.b.x, self.b.y))
+                           (self.a.p.x, self.a.p.y), 
+                           (self.b.p.x, self.b.p.y))
 
 
