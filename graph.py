1755d1577c30c8c1d2a9ca7b26b87a4bfab78092
Jan Keuchel <jan@keuchel.de>
2025-10-02 10:47:42 +0200

    Merged algorithms.py and graph.py into one file.

--- graph.py
+++ graph.py
@@ -78,7 +78,6 @@ class Graph:
     def _gen_mst(self, num_vertices=10) -> None:
         self._gen_fully_connected(num_vertices)
 
-        from algorithms import mst_prims
         mst = mst_prims(self)
 
         self._clear_edges()
@@ -130,3 +129,67 @@ class Graph:
             color = constants.FOREGROUND
         for v in self.V:
             v.draw(screen, draw_compact)
+
+
+# ------------------------------------
+# ----------- Algorithms  ------------
+# ------------------------------------
+
+def _all_eq(ints:List[int]) -> bool:
+    return all(x == ints[0] for x in ints)
+
+def mst_prims(G:Graph):
+    """
+    Only for undirected graphs.
+    """
+
+    union = Union(len(G.V))
+    edges = sorted(G.E, key=lambda e: e.weight, reverse=True)
+    mst: List[Edge] = []
+
+    for _ in range(len(G.V) - 1):
+
+        smallest_edge = edges.pop()
+        u = smallest_edge.a
+        v = smallest_edge.b
+
+        while union.get_representative(u.id) == union.get_representative(v.id):
+            smallest_edge = edges.pop()
+            u = smallest_edge.a
+            v = smallest_edge.b
+
+        union.union(u.id, v.id)
+        mst.append(smallest_edge)
+
+    return mst
+
+
+
+
+# ------------------------------------
+# --------- Data structures ----------
+# ------------------------------------
+
+class Union:
+    def __init__(self, n:int) -> None:
+        self._representatives: List[int] = list(range(n))
+
+    def union(self, u:int, v:int) -> None:
+        if u == v:
+            return
+        
+        rep_u = self._representatives[u]
+        rep_v = self._representatives[v]
+
+        (new_rep, to_replace) = (rep_u, rep_v) if rep_u < rep_v else (rep_v, rep_u)
+
+        for i in range(len(self._representatives)):
+            if self._representatives[i] == to_replace:
+                self._representatives[i] = new_rep
+
+    def get_representative(self, idx:int) -> int:
+        return self._representatives[idx]
+
+
+
+
