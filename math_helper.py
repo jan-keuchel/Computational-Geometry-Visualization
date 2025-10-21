9aab36001384f511b835f1c435dfa6341aeb45b2
Jan Keuchel <jan@keuchel.de>
2025-10-21 12:43:52 +0200

    Fix error in calculation. Accidentally used infinetely long lines instead of line segments.

--- math_helper.py
+++ math_helper.py
@@ -23,9 +23,13 @@ def line_segment_intersection(a1:Point, a2:Point, b1:Point, b2:Point) -> bool:
     return ((right_of(a1, a2, b1) * right_of(a1, a2, b2) < 0) and 
             (right_of(b1, b2, a1) * right_of(b1, b2, a2) < 0))
 
-def point_line_segment_intersection(a1:Point, a2:Point, b1:Point, b2:Point) -> Point:
-    # https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
 
+# https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment
+def point_line_segment_intersection(a1:Point, a2:Point, b1:Point, b2:Point, p:Point) -> bool:
+    """
+    `point_line_segment_intersection` calculates the point of intersection between
+    the lines a1 -- a2 and b1 -- b2. The resulting 
+    """
     x1: float = a1.x
     x2: float = a2.x
     x3: float = b1.x
@@ -36,9 +40,17 @@ def point_line_segment_intersection(a1:Point, a2:Point, b1:Point, b2:Point) -> P
     y3: float = b1.y
     y4: float = b2.y
 
-    px: float = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / \
-                ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
-    py: float = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / \
-                ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+    denom: float = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
+    if denom == 0:
+        return False
 
-    return Point(int(px), int(py))
+    t: float =  ((x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4)) / denom
+    u: float =  ((x1 - x2)*(y1 - y3) - (y1 - y2)*(x1 - x3)) / denom
+
+    if not (t >= 0 and t <= 1 and u >= 0 and u <= 1):
+        return False
+
+    p.x = int(x1 + t * (x2 - x1))
+    p.y = int(y1 + t * (y2 - y1))
+
+    return True
