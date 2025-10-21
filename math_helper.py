3e288b4c056ddd68313570c8bc7db4250c7af776
Jan Keuchel <jan@keuchel.de>
2025-10-21 12:27:28 +0200

    Add function to calculate intersection point of two segments.

--- math_helper.py
+++ math_helper.py
@@ -23,4 +23,22 @@ def line_segment_intersection(a1:Point, a2:Point, b1:Point, b2:Point) -> bool:
     return ((right_of(a1, a2, b1) * right_of(a1, a2, b2) < 0) and 
             (right_of(b1, b2, a1) * right_of(b1, b2, a2) < 0))
 
+def point_line_segment_intersection(a1:Point, a2:Point, b1:Point, b2:Point) -> Point:
+    # https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
 
+    x1: float = a1.x
+    x2: float = a2.x
+    x3: float = b1.x
+    x4: float = b2.x
+
+    y1: float = a1.y
+    y2: float = a2.y
+    y3: float = b1.y
+    y4: float = b2.y
+
+    px: float = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / \
+                ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+    py: float = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / \
+                ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+
+    return Point(int(px), int(py))
